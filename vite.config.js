0fa4f7a36ccf158487ddba519e6322508c3ae0da
Irina Ichim <onlyirina7@gmail.com>
2026-01-24 22:13:46 +0100

    Configure GitHub Actions for automatic deployment

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,18 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/API-Resilience-Wrapper/',
   root: './',
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'dashboard/index.html')
+        main: resolve(__dirname, 'index.html')
       }
     }
   },
   server: {
     port: 3000,
-    open: '/dashboard/index.html'
+    open: '/index.html'
   }
 });
